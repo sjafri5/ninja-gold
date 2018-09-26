14393e70fa49d2c01eb4641b97291c56e49a2c93
sjafri5 <raza.jafri.sy@gmail.com>
2018-09-26 15:03:01 -0500

    process_money route established

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/process_money', methods=['GET', 'POST'])
+def process_money():
+    print('foo')
+    if request.method == 'POST':
+        return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
