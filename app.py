2ff744cea2162cf9114a3428b04920cf0eda7e33
sjafri5 <raza.jafri.sy@gmail.com>
2018-09-26 15:52:18 -0500

    activities section reports the outcome of the form

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 import random
 import os
 
 def calculate_earnings(building):
     options = {
-            'farm' : {'min': 10, 'max': 21},
-            'cave' : {'min': 5, 'max': 11},
-            'house' : {'min': 2, 'max': 6},
-            'casino' : {'min': -50, 'max': 51},
+            'farm' : {'min': 10, 'max': 20},
+            'cave' : {'min': 5, 'max': 10},
+            'house' : {'min': 2, 'max': 5},
+            'casino' : {'min': -50, 'max': 50},
             }
 
     value_change = random.randint(options[building]['min'], options[building]['max']);
-    print('value_change')
     return value_change
 
 app = Flask(__name__)
@@ -21,15 +20,23 @@ app.secret_key = os.urandom(24)
 def index():
     gold_count = 0
     session['gold_count'] = gold_count
+    session['activities'] = []
     return render_template('index.html', gold_count=gold_count)
 
 @app.route('/process_money', methods=['GET', 'POST'])
 def process_money():
     if request.method == 'POST':
+        gold_count = session['gold_count']
         building = request.form['building']
+        activities = session['activities']
         value_change = calculate_earnings(building)
+        activities.append(value_change)
+        session['activities'] = activities
+
+
         print(value_change)
-        return render_template('index.html')
+        print(activities)
+        return render_template('index.html', gold_count=gold_count, activities=activities)
 
 
 if __name__ == '__main__':
